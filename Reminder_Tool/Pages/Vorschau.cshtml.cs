a7f0e3bcb899839ed0d7b35d70d1f1e9ac9c571f
Julian Scheibmayr <ScheibmayrJ180117@sus.htl-grieskirchen.at>
2024-01-08 10:16:06 +0100

    erste html fertig (struktur) alle gebrauchten componenten mit ids versehen am 11.  wird mit index.js angefangen

---
+++ Reminder_Tool/Pages/Vorschau.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Reminder_Tool.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
